b51dcc394b88d11a2b75c3d524f34bfe7baffbfe
Susant Kumar <susant@gmail.com>
2023-08-15 15:35:31 +0530

    RoomDatabase migration

--- app/src/main/java/com/zerocoder/ContactDatabase.kt
+++ app/src/main/java/com/zerocoder/ContactDatabase.kt
@@ -15,7 +15,7 @@ abstract class ContactDatabase : RoomDatabase() {
 
     companion object {
 
-        val migration_1_2 = object : Migration(1, 2) {
+        private val migration_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE contact ADD COLUMN isActive INTEGER NOT NULL DEFAULT(1)")
             }
